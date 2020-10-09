fd9ada0c7378e133c18f5309abb5b4780a48d7db
Brandon Leininger <contact@brandonleininger.fr>
2020-10-09 09:41:30 +0200

    WIP exercice-5 (non trouvé)

--- src/components/Shooting-star.vue
+++ src/components/Shooting-star.vue
@@ -1,9 +1,9 @@
 <template>
   <div 
     class="star shooting-star"
+    ref="shootingStar"
     :style="style"
   >
-
   </div>
 </template>
 
@@ -25,17 +25,39 @@ export default {
         left: ${this.randomX}px;
       `;
     }
+  },
+  methods: {
+  },
+  mounted() {
+    console.log('mounted')
+    console.log(this.$refs.shootingStar.classList)
+    this.$refs.shootingStar.classList = this.$refs.shootingStar.classList.value + ' launchingStar'
+    this.$refs.shootingStar.style.top = '0'
+    console.log(this.$refs.shootingStar.classList)
+
   }
 };
 </script>
 
 <style lang="scss" scoped>
+:root {
+  --translate-x: 0px;
+  --translate-y: 0px;
+}
+
 .star {
   position: absolute;
   background-color: red;
   border-radius: 100%;
-  height: 6px;
-  width: 6px;
+  height: 60px;
+  width: 60px;
+  margin-left: 0;
+  transition: all 2s ease-in-out;
+
+  &.launchingStar {
+    transform: translate(200px);
+  }
+
 }
 </style>
 
